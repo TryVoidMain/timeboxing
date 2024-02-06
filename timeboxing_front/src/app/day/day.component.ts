d15751c67de7c26b137b8630aa5fed9b43ab0331
TryVoidMain <tryvoidmain@gmail.com>
2024-02-06 22:01:19 +0300

    1. fixed display issue. 2. start modify job-component

--- timeboxing_front/src/app/day/day.component.ts
+++ timeboxing_front/src/app/day/day.component.ts
@@ -1,27 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InsightsComponent } from '../insights/insights.component';
 import { DayService } from '../../services/dayService';
 import { Day } from '../../types/Day';
 import { JobsListComponent } from '../jobs-list/jobs-list.component';
 import { DailyJobsComponent } from '../daily-jobs/daily-jobs.component';
 import { ScheduleComponent } from '../schedule/schedule.component';
-import { AsyncPipe, NgIf } from '@angular/common';
-import { Observable } from "rxjs";
+import { NgIf, AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 
 @Component({
   selector: 'app-day',
   standalone: true,
-  imports:  [InsightsComponent, JobsListComponent, DailyJobsComponent, ScheduleComponent, NgIf, AsyncPipe ],
+  imports:  [InsightsComponent, AsyncPipe,JobsListComponent, DailyJobsComponent, ScheduleComponent, NgIf ],
   templateUrl: './day.component.html',
   styleUrl: './day.component.css'
 })
-export class DayComponent {
-  currentDay$?: Observable<Day>;
-
+export class DayComponent implements OnInit {
+  currentDay$?: Observable<Day>; 
   constructor(private dayService: DayService) {}
 
-  ngOnInit(): void {
-    this.currentDay$ = this.dayService.GetDay(new Date);
+  public ngOnInit(): void {
+    this.currentDay$ = this.dayService.GetDay(new Date); 
   }
 }
--- timeboxing_front/src/app/job/job.component.ts
+++ timeboxing_front/src/app/job/job.component.ts
@@ -1,20 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Job } from '../../types/Job';
 import { NgIf } from '@angular/common';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-job',
   standalone: true,
-  imports: [ NgIf, ReactiveFormsModule ],
+  imports: [ NgIf ],
   templateUrl: './job.component.html',
   styleUrl: './job.component.css'
 })
 export class JobComponent {
   @Input() public job?: Job;
 
-  title = new FormControl(this.job!.title);
-  description = new FormControl(this.job!.description);
-
   constructor() {}
+
+  public onEditClicked() {
+
+  }
 }
